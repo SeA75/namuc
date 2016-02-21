db47c5720552ff59307c1e38bcdd78e311818881
SeA75 <andrea.turolla@gmail.com>
2016-02-21 23:51:17 +0100

    first release not tested of vector interface and implemetantio

---
+++ NamucMathLibrary/NamucMathLibrary/INamVector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamucMathLibrary
+{
+    /// <summary>
+    /// Namuc vector interface for vector
+    /// </summary>
+    public interface INamVector : IEnumerable
+    {
+        /// <summary>
+        /// Get number of components
+        /// </summary>
+        UInt32 Components { get; }
+
+        /// <summary>
+        /// Get or set the elements of the vector
+        /// </summary>
+        /// <param name="componentIndex"></param>
+        /// <returns></returns>
+        float this[int componentIndex]
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets the norm of the vector
+        /// </summary>
+        float Norm
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the normalize form of the vector.
+        /// </summary>
+        INamVector Normalized
+        {
+            get;
+        }
+
+    }
+    
+}
---
+++ NamucMathLibrary/NamucMathLibrary/NamVector.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace NamucMathLibrary
+{
+    
+
+    class NamVector : INamVector, ICloneable, IEnumerable
+    {
+       
+        /// <summary>
+        /// Elements of the N dimensional vector
+        /// </summary>
+        private readonly float[] _elements;
+
+        /// <summary>
+        /// NUmber of N components of the vector
+        /// </summary>
+        private uint _numOfComponents;
+
+        /// <summary>
+        /// Norm of the vector
+        /// </summary>
+        private float _norm;
+         
+
+        public uint Components
+        {
+            get { return _numOfComponents; }
+        }
+
+        public float this[int componentIndex]
+        {
+            get
+            {
+                return this[componentIndex];
+            }
+            set
+            {
+                this[componentIndex] = value;
+            }
+        }
+
+        
+        /// <summary>
+        /// Calculate the 2 based norm of the N vector
+        /// </summary>
+        public float Norm
+        {
+            get {
+                double cal = 0;
+                foreach (float item in _elements)
+                    cal += Math.Pow((float)item, (float)2);
+                _norm = (float)Math.Sqrt(cal);
+                return _norm; }
+        }
+
+        public INamVector Normalized
+        {
+            get {
+                // TODO da rivedere l'algoritmo
+                INamVector vect = (NamVector)this.Clone();
+                int index = 0;
+                foreach (float item in vect)
+                {
+                    vect[index] = item / _norm;
+                    index++;
+                }
+                return vect;
+                
+            }
+        }
+
+
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _elements.GetEnumerator();
+        }
+    }
+}
