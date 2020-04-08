3be65bb3f1c8ea78fba93a0d559f3b253a6a54ad
SeA75 <andrea.turolla@gmail.com>
2020-04-08 23:53:13 +0200

    first commit namuc namespace

---
+++ Namuc/Vector3D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Namuc
+{
+    public class Vector3D
+    {
+        public Vector3D(double ix, double iy, double iz)
+        {
+            x = ix;
+            y = iy;
+            z = iz;
+        }
+
+        public double x;
+
+        public double y;
+
+        public double z;
+    }
+}
